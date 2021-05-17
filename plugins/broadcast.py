ce4ddf93c1879ceba87b128f7e6786f4c082a414
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2021-05-17 12:56:16 +0530

    fix

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -24,7 +24,7 @@ async def bcast(client, message):
     )
 
 
-@Client.on_message(filters.command("stats") & filters.user(var.OWNER_ID))
+@Client.on_message(filters.command("stats") & filters.user(int(var.OWNER_ID)))
 async def gistat(_, message):
     al = get_all_chats()
     await message.reply_text(f"Total Chats in Database - {len(al)}", quote=True)
